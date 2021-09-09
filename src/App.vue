739022eb9e6b00145dbd7bf6e0a7364fd5f2f034
daiki-nishimura0 <nishimura-dai@deltanet.co.jp>
2021-09-09 17:59:37 +0900

    feat: practice

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,50 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div>
+    <h3>My TODO</h3>
+    <input v-model="newTodo" placeholder="Input here...">
+    <button v-on:click="addTodo()">追加</button>
+    <h5>ToDo List</h5>
+    <ul>
+      <li v-for="(todo, i) in todos" v-bind:key="i">
+        {{ todo }}
+        <button v-on:click="deleteTodo(todos.id)">削除</button>
+        <button v-on:click="PutTodo(todos)">編集</button>
+      </li>
+    </ul>    
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      todos: [],
+      newTodo: ""
+    }
+  },
+  created(){
+        axios.get('http://localhost:3000/todos')
+            .then(response => this.todos = response.data)
+            .catch(error => console.log(error))
+    },
+  methods: {
+    addTodo: function() {
+      axios.post('http://localhost:3000/todos',{newTodo: this.newTodo})
+        .then(response => this.newTodo.push(response))
+        .catch(error => console.log(error))    
+    },
+    deleteTodo: function() {
+      axios.delete('http://localhost:3000/todos/', {data: this.todos.id} )
+      .then(response => (response.data))
+      .catch(error => console.log(error))       
+    },
+    PutTodo: function(){
+      axios.put('http://localhost:3000/todos/' , {id: this.todos.Number} )
+      .then(response => (response.data))
+      .catch(error => console.log(error))  
     }
   }
 }
-</style>
+</script>
